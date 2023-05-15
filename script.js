8e6b2cabeaacec8382f37106f61e9f7c3a306a54
Tara Evans <tarajevans@hotmail.com>
2023-05-15 12:58:34 -0400

    finished JS

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
 var wins = 0;
-var losses = 0;
 var ties = 0;
+var losses = 0;
+
+// Array of options for computer to pick from
 var options = ["R", "P", "S"];
 
 var playGame = function() {
-var userChoice = windows.prompt("Enter your choice of R, P or S:");
+  // Ask user for their choice
+  var userChoice = window.prompt("Enter R, P, or S:");
 
-if (!userChoice) {
+  // If user pressed Cancel, immediately end function
+  if (!userChoice) {
     return;
-}
-userChoice = userChoice.toUppercase();
+  }
 
-var index = Math.floor(Math.random() * options.length);
-var computerChoice = options[index];
+  // Convert to uppercase to make comparisons easier
+  userChoice = userChoice.toUpperCase();
 
-windows.alert("The computer chose " + computerChoice);
+  // Get random index from array of options
+  var index = Math.floor(Math.random() * options.length);
+  var computerChoice = options[index];
 
-if (userChoice === computerChoice) {
+  window.alert("The computer chose " + computerChoice);
+
+  // If choices are the same, it's a tie
+  if (userChoice === computerChoice) {
     ties++;
-    windows.alert("It's a tie!");
-} else if (
+    window.alert("It's a tie!");
+
+  // Check every win condition for the player
+  } else if (
     (userChoice === "R" && computerChoice === "S") || 
     (userChoice === "P" && computerChoice === "R") || 
     (userChoice === "S" && computerChoice === "P")
-) {
+  ) {
     wins++;
     window.alert("You win!");
-} else {
+
+  // If above conditions failed, assume player lost
+  } else {
     losses++;
-    windows.alert("You lose!")
-}
-}
+    window.alert("You lost!");
+  }
+
+  // Print stats with line breaks
+  window.alert(
+    "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+  );
+
+  // Ask user to play again
+  var playAgain = window.confirm("Play again?");
+
+  // If user pressed OK, run the function again
+  if (playAgain) {
+    playGame();
+  }
+};
+
+// Run the game for the first time
+playGame();
